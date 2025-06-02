50bf9f0bcbfe68c1566ade99f6a235073d7c4ab4
Lucasrc22 <lucasrios2210@gmail.com>
2025-06-02 13:49:12 -0300

    iniciando app android

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'stockcontrol.app',
+  appName: 'StockControl',
+  webDir: 'dist'
+};
+
+export default config;
